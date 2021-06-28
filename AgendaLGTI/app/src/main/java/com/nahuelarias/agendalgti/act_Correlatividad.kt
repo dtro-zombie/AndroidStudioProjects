7d5c5a4d8b7870b9d4c6c1ef76627b930c99ef21
nahuel arias <ariasnahuel8@gmail.com>
2021-06-28 11:47:49 -0300

    ultima version estable

--- AgendaLGTI/app/src/main/java/com/nahuelarias/agendalgti/act_Correlatividad.kt
+++ AgendaLGTI/app/src/main/java/com/nahuelarias/agendalgti/act_Correlatividad.kt
@@ -3,11 +3,9 @@ package com.nahuelarias.agendalgti
 import android.annotation.SuppressLint
 import android.os.Build
 import android.os.Bundle
-import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_act_correlatividad.*
-import java.io.File
 
 class act_Correlatividad : AppCompatActivity() {
     @RequiresApi(Build.VERSION_CODES.O)
@@ -21,9 +19,11 @@ class act_Correlatividad : AppCompatActivity() {
 
         var bundle= intent.extras
         var Pos= bundle?.getInt("dt")
-
         val listaProductos = getMaterias()
         val producto = listaProductos[Pos as Int]
+
+
+
 
         var listacorrelativas = mutableListOf<String>()
 
@@ -60,48 +60,6 @@ class act_Correlatividad : AppCompatActivity() {
 
         }
 
-
-        swt_aprobado.setOnCheckedChangeListener { _ ,isChecked->
-            var i =0
-            var cont = 0
-
-            if(isChecked)
-            {
-
-
-
-                                                producto.Aprobada="true"
-                                                var temp=producto
-
-                                                listaProductos.remove(producto)
-                                                listaProductos.add(temp)
-
-
-                var materiastexto : MutableList<Materias>
-                                                    listaProductos.forEach {
-
-                                                        materiastexto.add(listaProductos[1])  }
-
-
-                                                val outString = listaProductos
-
-                                                File("src/main/res/raw/data.txt").printWriter().use { out -> out.println(outString) }
-                                                Toast.makeText(this, "FELICIDADES UNA MAS,FALTA MENOS!!! ", Toast.LENGTH_SHORT).show()
-
-
-
-
-
-
-            }else{
-
-                Toast.makeText(this, "TENES QUE APROBAR CORRELATIVAS!!", Toast.LENGTH_SHORT).show()
-
-            }
-
-
-
-        }
 
 
 
